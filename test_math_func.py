757d1a9a2ccb27533e30e8de632f39f4a33ad165
krishnan <mgkrish19@gmail.com>
2020-01-11 11:49:07 +0000

    testing

---
+++ test_math_func.py
@@ -0,0 +1,10 @@
+import math_func
+
+def test_add():
+  assert math_func.add(7, 3) == 10
+  assert math_func.add(7) == 9
+  assert math_func.add(5) == 7
+
+def test_product():
+  assert math_func.product(5, 3) == 15
+  assert math_func.product(5, 3) == 15  
