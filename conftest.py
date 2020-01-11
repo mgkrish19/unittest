9191cc6636d6fe9493fa98c504c656520695a18c
mgkrish19 <mgkrish19@gmail.com>
2020-01-11 17:27:03 +0000

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,15 +1,38 @@
-import pytest
-from pyspark.sql import SparkSession
+from pyspark import HiveContext
+from pyspark import SparkConf
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+
+def quiet_py4j():
+    """ turn down spark logging for the test context """
+    logger = logging.getLogger('py4j')
+    logger.setLevel(logging.WARN)
+
+@pytest.fixture(scope="session",
+            params=[pytest.mark.spark_local('local'),
+                    pytest.mark.spark_yarn('yarn')])
+def spark_context(request):
+    if request.param == 'local':
+        conf = (SparkConf()
+                .setMaster("local[2]")
+                .setAppName("pytest-pyspark-local-testing")
+                )
+    elif request.param == 'yarn':
+        conf = (SparkConf()
+                .setMaster("yarn-client")
+                .setAppName("pytest-pyspark-yarn-testing")
+                .set("spark.executor.memory", "1g")
+                .set("spark.executor.instances", 2)
+                )
+    request.addfinalizer(lambda: sc.stop())
+
+    sc = SparkContext(conf=conf)
+    return sc
 
 @pytest.fixture(scope="session")
-def spark_session(request):
-    """Fixture for creating a spark context."""
+def hive_context(spark_context):
+    return HiveContext(spark_context)
 
-    spark = (SparkSession
-             .builder
-             .master('local[2]')
-             .appName('pytest-pyspark-local-testing')
-             .enableHiveSupport()
-             .getOrCreate())
-    request.addfinalizer(lambda: spark.stop())
-    return spark
+@pytest.fixture(scope="session")
+def streaming_context(spark_context):
+    return StreamingContext(spark_context, 1)
