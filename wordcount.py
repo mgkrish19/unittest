3a30b647655a618bd2401a5fc0ab7ca7cd4a7f7f
krishnan <mgkrish19@gmail.com>
2020-01-11 17:37:50 +0000

    checking

--- wordcount.py
+++
@@ -1,7 +0,0 @@
-def do_word_counts(lines):
-    counts = (lines.flatMap(lambda x: x.split())
-                  .map(lambda x: (x, 1))
-                  .reduceByKey(lambda x, y: x+y)
-             ) 
-    results = {word: count for word, count in counts.collect()}
-    return results
