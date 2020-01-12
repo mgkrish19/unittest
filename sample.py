0186ba5aad2443679a69ac0e6a719b21c4254268
krishnan <mgkrish19@gmail.com>
2020-01-12 20:40:15 +0000

    remove

--- sample.py
+++
@@ -1,7 +0,0 @@
-df = spark.read.csv('s3://krishpocone/samplefile.csv', header=False, inferSchema=True, sep=',')
-df.count()
-df.dtypes
-df.take(5)
-df_t = df.select('_c0','_c1','_c2', '_c3', '_c4', '_c5')
-df_t.take(1)
-df_t = df_t.withColumnRenamed('_c0', 'newid').withColumnRenamed('_c1', 'newcolumn')
