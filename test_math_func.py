2d8b930dee69b2775957ce39705e0926510f32dd
mgkrish19 <mgkrish19@gmail.com>
2020-01-13 09:11:21 +0000

    Update test_math_func.py

--- test_math_func.py
+++ test_math_func.py
@@ -4,6 +4,7 @@ def test_add():
   assert math_func.add(7, 3) == 10
   assert math_func.add(7) == 9
   assert math_func.add(5) == 7
+  assert math_func.add(3) == 5
   
 def test_product():
   assert math_func.product(5, 3) == 15
