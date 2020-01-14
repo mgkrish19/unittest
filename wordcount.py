7565e70c02799f012a69793f46e3a1e4bff30ad4
krishnan <mgkrish19@gmail.com>
2020-01-14 13:04:23 +0000

    new

---
+++ wordcount.py
@@ -0,0 +1,9 @@
+def do_word_counts(lines):
+    counts = (lines.flatMap(lambda x: x.split())
+                  .map(lambda x: (x, 1))
+                  .reduceByKey(lambda x, y: x+y)
+             ) 
+    results = {word: count for word, count in counts.collect()}
+	#results = dict(counts.collect())
+    return results	
+
---
+++ wordcount_test.py
@@ -0,0 +1,16 @@
+import pytest
+import wordcount
+#pytestmark = pytest.mark.usefixtures("spark_context")
+
+@pytest.mark.usefixtures("spark_context") 
+def test_do_word_counts(spark_context):
+    test_input = [
+        ' hello spark ',
+        ' hello again spark spark'
+    ]
+
+    input_rdd = spark_context.parallelize(test_input, 1)
+    results = wordcount.do_word_counts(input_rdd)
+
+    expected_results = {'hello':2, 'spark':3, 'again':1}  
+    assert results == expected_results
